2610e49474a7d0b74bca4aeb110c056aa90c6cf7
Markus <barikal1603@gmail.com>
2024-11-08 06:16:47 +0300

    Программа готова

---
+++ lab_1.py
@@ -0,0 +1,9 @@
+"""
+Программа для вывода Hello, World
+"""
+def print_hello_world():
+    """Функция для вывода строки Hello, World!"""
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    print_hello_world()
